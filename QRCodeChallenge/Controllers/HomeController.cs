ad499fb8c232a6b764eba793f861824eefe08b88
jean claude Misigaro <mjeanclaude23@gmail.com>
2023-01-27 17:47:01 +0200

    Add project files.

---
+++ QRCodeChallenge/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QRCodeChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("http://fnactvs.com/index.html?SystemDeviceID=WZVORA", QRCodeGenerator.ECCLevel.Q);
+            QRCoder.QRCode qrCode = new QRCoder.QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            return View();
+        }
+    }
+}
